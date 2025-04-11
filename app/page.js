6ae1d5b941ccb43c0a0fac82bf18242c9f50f93b
purwar1997 <shubhampurwar511@gmail.com>
2025-04-11 11:11:02 +0530

    chore: update package version

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   useEffect(() => {
     const getTranslationsFromS3 = async () => {
       try {
-        await LanguageService.initialize('fr');
+        await LanguageService.initialize();
         const allTranslations = LanguageService.translate;
         console.log('All translations:', allTranslations);
         console.log('Signup heading:', allTranslations['signup_page_heading']);
