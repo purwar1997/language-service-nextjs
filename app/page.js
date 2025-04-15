ea50bf34cfebaad8fd0e829b886704d795ce13fb
purwar1997 <shubhampurwar511@gmail.com>
2025-04-15 22:08:16 +0530

    Add config files

--- app/page.js
+++ app/page.js
@@ -2,11 +2,16 @@
 
 import { useEffect } from 'react';
 import { LanguageService } from '@tx/language-service';
+import { ENV } from '@/lib/env';
+import config from '../config.json';
 
 export default function Home() {
   useEffect(() => {
     const getTranslationsFromS3 = async () => {
       try {
+        console.log('ENV:', ENV);
+        console.log('Config:', config);
+
         await LanguageService.initialize();
         const allTranslations = LanguageService.translate;
         console.log('All translations:', allTranslations);
